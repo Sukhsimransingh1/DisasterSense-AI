879764860eb29e66a22afc1100aa889d8bb9490b
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2026-06-15 21:31:07 +0530

    Fix map rendering and deployment issues

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import L from "leaflet";
+
 import "leaflet/dist/leaflet.css";
 
 
@@ -19,16 +19,9 @@ import {
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import StatCard from "../components/StatCard";
-delete (L.Icon.Default.prototype as any)._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
+
+
 interface Incident {
   id: number;
   incident_type: string;
